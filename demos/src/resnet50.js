6a5cd57f7d8687ac619006e35520c66ceb7ed6e0
Leon Chen <leon@md.ai>
2016-10-06 05:06:29 -0400

    update ResNet50 ImageNet demo

--- demos/src/resnet50.js
+++ demos/src/resnet50.js
@@ -1,4 +1,4 @@
-/* global Vue */
+/* global Vue, loadImage */
 import './resnet50.css'
 
 import * as utils from './utils'
@@ -37,12 +37,14 @@ export const ResNet50 = Vue.extend({
     return {
       model: new KerasJS.Model(MODEL_CONFIG),
       modelLoading: true,
-      imageURL: '',
+      imageURLInput: null,
+      imageURLSelect: null,
       imageURLSelectList: [
-        { name: 'cat', value: 'URL1' },
+        { name: 'cat', value: 'http://i.imgur.com/CzXTtJV.jpg' },
         { name: 'dog', value: 'URL2' }
       ],
-      input: new Float32Array(224 * 224 * 3),
+      imageLoading: false,
+      imageLoadingError: false,
       output: new Float32Array(1000),
       layerResultImages: [],
       layerDisplayConfig: LAYER_DISPLAY_CONFIG,
@@ -61,7 +63,7 @@ export const ResNet50 = Vue.extend({
     this.model.initialize()
     this.model.ready().then(() => {
       this.modelLoading = false
-      this.getIntermediateResults()
+      //this.getIntermediateResults()
     })
   },
 
@@ -69,6 +71,69 @@ export const ResNet50 = Vue.extend({
 
     toggleGpu: function () {
       this.model.gpu = !this.useGpu
+    },
+
+    imageURLInputChanged: function (e) {
+      this.imageURLSelect = null
+      this.loadImageToCanvas(this.imageURLInput)
+    },
+
+    imageURLSelectChanged: function (e) {
+      this.imageURLInput = this.imageURLSelect
+      this.loadImageToCanvas(this.imageURLSelect)
+    },
+
+    loadImageToCanvas: function (url) {
+      this.imageLoading = true
+      loadImage(
+        url,
+        img => {
+          if (img.type === 'error') {
+            this.imageLoadingError = true
+            this.imageLoading = false
+          } else {
+            // load image data onto input canvas
+            const ctx = document.getElementById('input-canvas').getContext('2d')
+            ctx.drawImage(img, 0, 0)
+            this.imageLoadingError = false
+            this.imageLoading = false
+            // model predict
+            this.runModel()
+          }
+        },
+        {
+          maxWidth: 224,
+          maxHeight: 224,
+          cover: true,
+          crop: true,
+          canvas: true,
+          crossOrigin: 'Anonymous'
+        }
+      )
+    },
+
+    runModel: function () {
+      const ctx = document.getElementById('input-canvas').getContext('2d')
+      const imageData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height)
+      const { data, width, height } = imageData
+
+      // data processing
+      // see https://github.com/fchollet/keras/blob/master/keras/applications/imagenet_utils.py
+      let dataProcessed = new Float32Array(width * height * 3)
+      for (let i = 0, len = data.length; i < len; i += 4) {
+        // RGB -> BGR
+        dataProcessed[i / 4 + 2] = data[i] - 103.939
+        dataProcessed[i / 4 + 1] = data[i + 1] - 116.779
+        dataProcessed[i / 4] = data[i + 2] - 123.68
+      }
+
+      const inputData = {
+        'input_1': dataProcessed
+      }
+      const outputData = this.model.predict(inputData)
+      this.output = outputData['fc1000']
+      console.log(this.output)
+      //this.getIntermediateResults()
     },
 
     getIntermediateResults: function () {
@@ -101,9 +166,9 @@ export const ResNet50 = Vue.extend({
       this.layerResultImages.forEach((result, layerNum) => {
         const scalingFactor = this.layerDisplayConfig[result.name].scalingFactor
         result.images.forEach((image, imageNum) => {
-          let ctx = document.getElementById(`intermediate-result-${layerNum}-${imageNum}`).getContext('2d')
+          const ctx = document.getElementById(`intermediate-result-${layerNum}-${imageNum}`).getContext('2d')
           ctx.putImageData(image, 0, 0)
-          let ctxScaled = document.getElementById(`intermediate-result-${layerNum}-${imageNum}-scaled`).getContext('2d')
+          const ctxScaled = document.getElementById(`intermediate-result-${layerNum}-${imageNum}-scaled`).getContext('2d')
           ctxScaled.save()
           ctxScaled.scale(scalingFactor, scalingFactor)
           ctxScaled.clearRect(0, 0, ctxScaled.canvas.width, ctxScaled.canvas.height)
@@ -117,7 +182,7 @@ export const ResNet50 = Vue.extend({
       this.layerResultImages.forEach((result, layerNum) => {
         const scalingFactor = this.layerDisplayConfig[result.name].scalingFactor
         result.images.forEach((image, imageNum) => {
-          let ctxScaled = document.getElementById(`intermediate-result-${layerNum}-${imageNum}-scaled`).getContext('2d')
+          const ctxScaled = document.getElementById(`intermediate-result-${layerNum}-${imageNum}-scaled`).getContext('2d')
           ctxScaled.save()
           ctxScaled.scale(scalingFactor, scalingFactor)
           ctxScaled.clearRect(0, 0, ctxScaled.canvas.width, ctxScaled.canvas.height)
