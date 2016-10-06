d7988600b83c532df0f536dbf53a1d442eec52d2
Leon Chen <leon@md.ai>
2016-10-05 21:40:08 -0400

    working convolutional VAE demo

--- demos/src/mnist-vae.js
+++ demos/src/mnist-vae.js
@@ -2,7 +2,6 @@
 
 import './mnist-vae.css'
 
-import debounce from 'lodash/debounce'
 import * as utils from './utils'
 
 const MODEL_CONFIG = {
@@ -26,11 +25,15 @@ if (process.env.NODE_ENV === 'production') {
 
 const LAYER_DISPLAY_CONFIG = {
   'dense_10': {
-    heading: 'ReLU activation, output dimensions = 128',
+    heading: 'input dimensions = 2, output dimensions = 128, ReLU activation',
     scalingFactor: 2
   },
   'dense_11': {
     heading: 'ReLU activation, output dimensions = 25088 (64 x 14 x 14)',
+    scalingFactor: 2
+  },
+  'reshape_4': {
+    heading: '',
     scalingFactor: 2
   },
   'deconvolution2d_10': {
@@ -66,21 +69,41 @@ export const MnistVae = Vue.extend({
     <div class="columns input-output">
       <div class="column input-column">
         <div class="input-container">
-          <div class="input-label">Move around the latent space <span class="arrow">⤸</span></div>
+          <div class="input-label">Click around the latent space <span class="arrow">⤸</span></div>
           <div class="canvas-container">
             <canvas
               id="input-canvas" width="200" height="200"
               @mouseenter="activateCrosshairs"
               @mouseleave="deactivateCrosshairs"
-              @mousemove="drawCrosshairs"
-              @click="selectCoordinate"
-              @touchend="selectCoordinate"
+              @mousemove="draw"
+              @click="selectCoordinates"
+              @touchend="selectCoordinates"
             ></canvas>
+            <div class="axis x-axis">
+              <span>-1.5</span>
+              <span>x</span>
+              <span>1.5</span>
+            </div>
+            <div class="axis y-axis">
+              <span>-1.5</span>
+              <span>y</span>
+              <span>1.5</span>
+            </div>
           </div>
         </div>
       </div>
-      <div class="column controls-column">
+      <div class="column is-2 controls-column">
         <mdl-switch :checked.sync="useGpu" @click="toggleGpu">Use GPU</mdl-switch>
+        <div class="coordinates">
+          <div class="coordinates-x">x: {{ inputCoordinates[0] < 0 ? inputCoordinates[0].toFixed(2) : inputCoordinates[0].toFixed(3) }}</div>
+          <div class="coordinates-y">y: {{ inputCoordinates[1] < 0 ? inputCoordinates[1].toFixed(2) : inputCoordinates[1].toFixed(3) }}</div>
+        </div>
+      </div>
+      <div class="column output-column">
+        <div class="output">
+          <canvas id="output-canvas-scaled" width="150" height="150"></canvas>
+          <canvas id="output-canvas" width="27" height="27"></canvas>
+        </div>
       </div>
     </div>
     <div class="layer-results-container">
@@ -115,10 +138,10 @@ export const MnistVae = Vue.extend({
     return {
       model: new KerasJS.Model(MODEL_CONFIG),
       modelLoading: true,
-      input: new Float32Array(2),
       output: new Float32Array(27 * 27),
       crosshairsActivated: false,
-      coordinates: [0, 0],
+      inputCoordinates: [0, 0],
+      position: [100, 100],
       layerResultImages: [],
       layerDisplayConfig: LAYER_DISPLAY_CONFIG,
       useGpu: MODEL_CONFIG.gpu
@@ -140,7 +163,15 @@ export const MnistVae = Vue.extend({
     })
   },
 
+  ready: function () {
+    this.drawPosition()
+  },
+
   methods: {
+
+    toggleGpu: function () {
+      this.model.gpu = !this.useGpu
+    },
 
     activateCrosshairs: function (e) {
       this.crosshairsActivated = true
@@ -148,27 +179,40 @@ export const MnistVae = Vue.extend({
 
     deactivateCrosshairs: function (e) {
       this.crosshairsActivated = false
+      this.draw(e)
     },
 
-    drawCrosshairs: function (e) {
-      if (!this.crosshairsActivated) return
-
+    draw: function (e) {
       const [x, y] = this.getEventCanvasCoordinates(e)
       const ctx = document.getElementById('input-canvas').getContext('2d')
       ctx.clearRect(0, 0, 200, 200)
-      ctx.strokeStyle = '#1BBC9B'
+
+      this.drawPosition()
+
+      if (this.crosshairsActivated) {
+        ctx.strokeStyle = '#1BBC9B'
+        ctx.beginPath()
+        ctx.moveTo(x, 0)
+        ctx.lineTo(x, 200)
+        ctx.stroke()
+        ctx.beginPath()
+        ctx.moveTo(0, y)
+        ctx.lineTo(200, y)
+        ctx.stroke()
+      }
+    },
+
+    drawPosition: function () {
+      const ctx = document.getElementById('input-canvas').getContext('2d')
+      ctx.clearRect(0, 0, 200, 200)
+      ctx.fillStyle = '#E87E04'
       ctx.beginPath()
-      ctx.moveTo(x, 0)
-      ctx.lineTo(x, 200)
-      ctx.stroke()
-      ctx.beginPath()
-      ctx.moveTo(0, y)
-      ctx.lineTo(200, y)
-      ctx.stroke()
+      ctx.arc(...this.position, 5, 0, Math.PI * 2, true)
+      ctx.closePath()
+      ctx.fill()
     },
 
     getEventCanvasCoordinates: function (e) {
-      const borderSize = 15
       let { clientX, clientY } = e
       // for touch event
       if (e.touches && e.touches.length) {
@@ -178,20 +222,45 @@ export const MnistVae = Vue.extend({
 
       const canvas = document.getElementById('input-canvas')
       const { left, top } = canvas.getBoundingClientRect()
-      const [x, y] = [clientX - left - borderSize, clientY - top - borderSize]
+      const [x, y] = [clientX - left, clientY - top]
       return [x, y]
     },
 
-    selectCoordinate: function (e) {
+    selectCoordinates: function (e) {
+      const [x, y] = this.getEventCanvasCoordinates(e)
+      if (!this.model.isRunning) {
+        this.position = [x, y]
+        this.inputCoordinates = [x * 3 / 200 - 1.5, y * 3 / 200 - 1.5]
+        this.draw(e)
+        const inputData = {
+          'input_4': new Float32Array(this.inputCoordinates)
+        }
+        const outputData = this.model.predict(inputData)
+        this.output = outputData['convolution2d_8']
+        this.drawOutput()
+        this.getIntermediateResults()
+      }
+    },
 
+    drawOutput: function () {
+      const ctx = document.getElementById('output-canvas').getContext('2d')
+      ctx.putImageData(utils.image2Darray(this.output, 27, 27), 0, 0)
+
+      // scaled up
+      // const ctxScaled = document.getElementById('output-canvas-scaled').getContext('2d')
+      // ctxScaled.save()
+      // ctxScaled.scale(28 / ctxCenterCrop.canvas.width, 28 / ctxCenterCrop.canvas.height)
+      // ctxScaled.clearRect(0, 0, ctxCenterCrop.canvas.width, ctxCenterCrop.canvas.height)
+      // ctxScaled.drawImage(document.getElementById('output-canvas-centercrop'), 0, 0)
+      // const imageDataScaled = ctxScaled.getImageData(0, 0, ctxScaled.canvas.width, ctxScaled.canvas.height)
+      // ctxScaled.restore()
     },
 
     getIntermediateResults: function () {
       let results = []
       for (let [name, layer] of this.model.modelLayersMap.entries()) {
-        if (name === 'input') continue
-
         const layerClass = layer.layerClass || ''
+        if (layerClass === 'InputLayer') continue
 
         let images = []
         if (layer.result && layer.result.tensor.shape.length === 3) {
@@ -240,10 +309,6 @@ export const MnistVae = Vue.extend({
           ctxScaled.restore()
         })
       })
-    },
-
-    toggleGpu: function () {
-      this.model.gpu = !this.useGpu
     }
   }
 })
