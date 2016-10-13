f2ff207a95e8c94096da88f1aad648c3191c200c
Leon Chen <leon@md.ai>
2016-10-13 18:27:47 -0400

    update production build

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import Model from './Model'
 import Tensor from './Tensor'
 import * as activations from './activations'
 import * as layers from './layers'
-
-let testUtils
-if (process.env.NODE_ENV !== 'production') {
-  testUtils = require('./testUtils')
-}
+import * as testUtils from './testUtils'
 
 export {
   Model,
