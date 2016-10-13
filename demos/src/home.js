22ed26238b5bdfb28c80f4567eae3851b68c8791
Leon Chen <leon@md.ai>
2016-10-13 18:51:31 -0400

    update demos home links

--- demos/src/home.js
+++ demos/src/home.js
@@ -27,22 +27,27 @@ const DEMO_INFO_DEV = [
 const DEMO_INFO_PROD = [
   {
     title: 'Basic Convnet for MNIST',
+    path: 'mnist-cnn',
     imagePath: 'demos/assets/mnist-cnn.png'
   },
   {
     title: 'Convolutional Variational Autoencoder, trained on MNIST',
+    path: 'mnist-vae',
     imagePath: 'demos/assets/mnist-vae.png'
   },
   {
     title: '50-layer Residual Network, trained on ImageNet',
+    path: 'resnet50',
     imagePath: 'demos/assets/resnet50.png'
   },
   {
     title: 'Inception V3, trained on ImageNet',
+    path: 'inception-v3',
     imagePath: 'demos/assets/inception-v3.png'
   },
   {
     title: 'Bidirectional LSTM for IMDB sentiment classification',
+    path: 'imdb-bidirectional-lstm',
     imagePath: 'demos/assets/imdb-bidirectional-lstm.png'
   }
 ]
