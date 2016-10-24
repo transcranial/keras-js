27d4588c909635f1a6b153a1e8601cec498b4757
Leon Chen <leon@md.ai>
2016-10-24 00:26:57 -0400

    update builds

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
 const canvas = document.createElement('canvas')
 const gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl')
 
-if(!gl) {
-	throw new Error('Unable to initialize WebGL. Your browser may not support it.')
+if (!gl) {
+  throw new Error('Unable to initialize WebGL. Your browser may not support it.')
 }
 
 export const MAX_TEXTURE_SIZE = gl.getParameter(gl.MAX_TEXTURE_SIZE)
