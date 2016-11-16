c6e12f9c4ca59b931f48d5b0166d5a76f17bb517
M Clark <wassname@users.noreply.github.com>
2016-11-16 13:09:39 +0800

    Add Cropping1D,2D,3D (#28)
    * Added failing test for convolutional1D with input (16,1)
    
    * bugfix
    
    * Add Cropping1D,2D,3D
    
    * Update readme
    
    * update build
    
    * Revert "bugfix"
    
    This reverts commit e76961221d7937910763c4edaf97745b28a5ff61.
    
    * Revert "Added failing test for convolutional1D with input (16,1)"
    
    This reverts commit 5c91ed8eb78de7859e83d0632fc892f347914e5b.

---
+++ test/convolutional/Cropping1D.js
@@ -0,0 +1,48 @@
+/* eslint-env browser, mocha */
+
+describe('convolutional layer: Cropping1D', function () {
+  const assert = chai.assert
+  const styles = testGlobals.styles
+  const logTime = testGlobals.logTime
+  const stringifyCondensed = testGlobals.stringifyCondensed
+  const approxEquals = KerasJS.testUtils.approxEquals
+  const layers = KerasJS.layers
+
+  before(function () {
+    console.log('\n%cconvolutional layer: Cropping1D', styles.h1)
+  })
+
+  it(`[convolutional.Cropping1D.0] cropping (1,1) on 6x4 input`, function () {
+    const key = `convolutional.Cropping1D.0`
+    console.log(`\n%c[${key}] cropping (1,1) on 6x4 input`, styles.h3)
+    let testLayer = new layers.Cropping1D({ cropping: [1,1] })
+    let t = new KerasJS.Tensor(TEST_DATA[key].input.data, TEST_DATA[key].input.shape)
+    console.log('%cin', styles.h4, stringifyCondensed(t.tensor))
+    const startTime = performance.now()
+    t = testLayer.call(t)
+    const endTime = performance.now()
+    console.log('%cout', styles.h4, stringifyCondensed(t.tensor))
+    logTime(startTime, endTime)
+    const dataExpected = new Float32Array(TEST_DATA[key].expected.data)
+    const shapeExpected = TEST_DATA[key].expected.shape
+    assert.deepEqual(t.tensor.shape, shapeExpected)
+    assert.isTrue(approxEquals(t.tensor, dataExpected))
+  })
+
+  it(`[convolutional.Cropping1D.1] cropping (2,3) on 6x4 input`, function () {
+    const key = `convolutional.Cropping1D.1`
+    console.log(`\n%c[${key}] cropping (2,3) on 6x4 input`, styles.h3)
+    let testLayer = new layers.Cropping1D({ cropping: [2,3] })
+    let t = new KerasJS.Tensor(TEST_DATA[key].input.data, TEST_DATA[key].input.shape)
+    console.log('%cin', styles.h4, stringifyCondensed(t.tensor))
+    const startTime = performance.now()
+    t = testLayer.call(t)
+    const endTime = performance.now()
+    console.log('%cout', styles.h4, stringifyCondensed(t.tensor))
+    logTime(startTime, endTime)
+    const dataExpected = new Float32Array(TEST_DATA[key].expected.data)
+    const shapeExpected = TEST_DATA[key].expected.shape
+    assert.deepEqual(t.tensor.shape, shapeExpected)
+    assert.isTrue(approxEquals(t.tensor, dataExpected))
+  })
+})
---
+++ test/convolutional/Cropping3D.js
@@ -0,0 +1,82 @@
+/* eslint-env browser, mocha */
+
+describe('convolutional layer: Cropping3D', function () {
+  const assert = chai.assert
+  const styles = testGlobals.styles
+  const logTime = testGlobals.logTime
+  const stringifyCondensed = testGlobals.stringifyCondensed
+  const approxEquals = KerasJS.testUtils.approxEquals
+  const layers = KerasJS.layers
+
+  before(function () {
+    console.log('\n%cconvolutional layer: Cropping3D', styles.h1)
+  })
+
+  it(`[convolutional.Cropping3D.0] cropping ((1,1), (1,1), (1,1)) on 3x5x3x3 input, dim_ordering=tf`, function () {
+    const key = `convolutional.Cropping3D.0`
+    console.log(`\n%c[${key}] cropping ((1,1), (1,1), (1,1)) on 3x5x3x3 input, dim_ordering=tf`, styles.h3)
+    let testLayer = new layers.Cropping3D({ cropping: [[1, 1], [1, 1], [1,1]], dimOrdering: 'tf' })
+    let t = new KerasJS.Tensor(TEST_DATA[key].input.data, TEST_DATA[key].input.shape)
+    console.log('%cin', styles.h4, stringifyCondensed(t.tensor))
+    const startTime = performance.now()
+    t = testLayer.call(t)
+    const endTime = performance.now()
+    console.log('%cout', styles.h4, stringifyCondensed(t.tensor))
+    logTime(startTime, endTime)
+    const dataExpected = new Float32Array(TEST_DATA[key].expected.data)
+    const shapeExpected = TEST_DATA[key].expected.shape
+    assert.deepEqual(t.tensor.shape, shapeExpected)
+    assert.isTrue(approxEquals(t.tensor, dataExpected))
+  })
+
+  it(`[convolutional.Cropping3D.0] cropping ((1,1), (1,1), (1,1)) on 3x5x3x3 input, dim_ordering=th`, function () {
+    const key = `convolutional.Cropping3D.1`
+    console.log(`\n%c[${key}] cropping ((1,1), (1,1), (1,1)) on 3x5x3x3 input, dim_ordering=th`, styles.h3)
+    let testLayer = new layers.Cropping3D({ cropping: [[1, 1], [1, 1], [1,1]], dimOrdering: 'th' })
+    let t = new KerasJS.Tensor(TEST_DATA[key].input.data, TEST_DATA[key].input.shape)
+    console.log('%cin', styles.h4, stringifyCondensed(t.tensor))
+    const startTime = performance.now()
+    t = testLayer.call(t)
+    const endTime = performance.now()
+    console.log('%cout', styles.h4, stringifyCondensed(t.tensor))
+    logTime(startTime, endTime)
+    const dataExpected = new Float32Array(TEST_DATA[key].expected.data)
+    const shapeExpected = TEST_DATA[key].expected.shape
+    assert.deepEqual(t.tensor.shape, shapeExpected)
+    assert.isTrue(approxEquals(t.tensor, dataExpected))
+  })
+
+  it(`[convolutional.Cropping3D.2] cropping (3,2), (2,1), (2,3)) on 7x6x6x6 input, dim_ordering=tf`, function () {
+    const key = `convolutional.Cropping3D.2`
+    console.log(`\n%c[${key}] cropping (3,2), (2,1), (2,3)) on 7x6x6x6 input, dim_ordering=tf`, styles.h3)
+    let testLayer = new layers.Cropping3D({ cropping: [[3,2],[2,1],[2,3]], dimOrdering: 'tf' })
+    let t = new KerasJS.Tensor(TEST_DATA[key].input.data, TEST_DATA[key].input.shape)
+    console.log('%cin', styles.h4, stringifyCondensed(t.tensor))
+    const startTime = performance.now()
+    t = testLayer.call(t)
+    const endTime = performance.now()
+    console.log('%cout', styles.h4, stringifyCondensed(t.tensor))
+    logTime(startTime, endTime)
+    const dataExpected = new Float32Array(TEST_DATA[key].expected.data)
+    const shapeExpected = TEST_DATA[key].expected.shape
+    assert.deepEqual(t.tensor.shape, shapeExpected)
+    assert.isTrue(approxEquals(t.tensor, dataExpected))
+  })
+
+  it(`[convolutional.Cropping3D.2] cropping (3,2), (2,1), (2,3)) on 7x6x6x6 input, dim_ordering=th`, function () {
+    const key = `convolutional.Cropping3D.3`
+    console.log(`\n%c[${key}] cropping (3,2), (2,1), (2,3)) on 7x6x6x6 input, dim_ordering=th`, styles.h3)
+    let testLayer = new layers.Cropping3D({ cropping: [[3,2],[2,1],[2,3]], dimOrdering: 'th' })
+    let t = new KerasJS.Tensor(TEST_DATA[key].input.data, TEST_DATA[key].input.shape)
+    console.log('%cin', styles.h4, stringifyCondensed(t.tensor))
+    const startTime = performance.now()
+    t = testLayer.call(t)
+    const endTime = performance.now()
+    console.log('%cout', styles.h4, stringifyCondensed(t.tensor))
+    logTime(startTime, endTime)
+    const dataExpected = new Float32Array(TEST_DATA[key].expected.data)
+    const shapeExpected = TEST_DATA[key].expected.shape
+    assert.deepEqual(t.tensor.shape, shapeExpected)
+    assert.isTrue(approxEquals(t.tensor, dataExpected))
+  })
+})
