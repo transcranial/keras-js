8a8d181dbb52fcd1e2f31b187350dd4010f59388
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2016-11-24 19:22:57 -0500

    Add GaussianDropout and Noise layers

---
+++ src/layers/noise/GaussianDropout.js
@@ -0,0 +1,32 @@
+import Layer from '../../Layer'
+
+/**
+ * GaussianDropout layer class
+ * Note that this layer is here only for compatibility purposes,
+ * as it's only active during training phase.
+ */
+export default class GaussianDropout extends Layer {
+  /**
+   * Creates a GaussianDropout layer
+   * @param {number} attrs.p - fraction of the input units to drop (between 0 and 1)
+   */
+  constructor (attrs = {}) {
+    super(attrs)
+    this.layerClass = 'GaussianDropout'
+
+    const {
+      p = 0.5
+    } = attrs
+
+    this.p = Math.min(Math.max(0, p), 1)
+  }
+
+  /**
+   * Method for layer computational logic
+   * @param {Tensor} x
+   * @returns {Tensor} x
+   */
+  call (x) {
+    return x
+  }
+}
---
+++ src/layers/noise/GaussianNoise.js
@@ -0,0 +1,31 @@
+import Layer from '../../Layer'
+
+/**
+ * GaussianNoise layer class
+ * Note that this layer is here only for compatibility purposes,
+ * as it's only active during training phase.
+ */
+export default class GaussianNoise extends Layer {
+  /**
+   * Creates a GaussianNoise layer
+   * @param {number} attrs.p - fraction of the input units to drop (between 0 and 1)
+   */
+  constructor (attrs = {}) {
+    super(attrs)
+    this.layerClass = 'GaussianNoise'
+
+    const {
+      sigma = 0
+    } = attrs
+
+  }
+
+  /**
+   * Method for layer computational logic
+   * @param {Tensor} x
+   * @returns {Tensor} x
+   */
+  call (x) {
+    return x
+  }
+}
