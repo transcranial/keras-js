69b7321fde8bf7bc252d7202641beaf201827347
Leon Chen <leon@md.ai>
2017-05-17 13:34:23 -0400

    update core/embedding/noise/activation layers test data

--- test/layers/core/Dropout_data.js
+++ test/layers/core/Dropout_data.js
@@ -5,11 +5,11 @@
 ;(function() {
   var DATA = {
     'core.Dropout.0': {
-      input: { data: [0, 0.2, 0.5, -0.1, 1, 2], shape: [6] },
       weights: [
         { data: [0.1, 0.4, 0.5, 0.1, 1.0, -2.0, 0.0, 0.3, 0.2, 0.1, 3.0, 0.0], shape: [6, 2] },
         { data: [0.5, 0.7], shape: [2] }
       ],
+      input: { data: [0, 0.2, 0.5, -0.1, 1, 2], shape: [6] },
       expected: { data: [7.3, -0.21], shape: [2] }
     }
   }
