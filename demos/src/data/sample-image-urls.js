46c7d6072bca564d3118135841bd3dd5a666213f
Leon Chen <leon@md.ai>
2017-05-18 20:11:55 -0400

    ensure all urls are https

--- demos/src/data/sample-image-urls.js
+++ demos/src/data/sample-image-urls.js
@@ -1,10 +1,10 @@
 export const IMAGE_URLS = [
-  { name: 'cat', value: 'http://i.imgur.com/CzXTtJV.jpg' },
-  { name: 'dog', value: 'http://i.imgur.com/OB0y6MR.jpg' },
+  { name: 'cat', value: 'https://i.imgur.com/CzXTtJV.jpg' },
+  { name: 'dog', value: 'https://i.imgur.com/OB0y6MR.jpg' },
   { name: 'cheetah', value: 'https://farm2.staticflickr.com/1533/26541536141_41abe98db3_z_d.jpg' },
   { name: 'goldfish', value: 'https://farm2.staticflickr.com/1301/1349366952_982df2276f_z_d.jpg' },
   { name: 'bird', value: 'https://farm4.staticflickr.com/3075/3168662394_7d7103de7d_z_d.jpg' },
-  { name: 'bridge', value: 'http://i.imgur.com/Bvke53p.jpg' },
+  { name: 'bridge', value: 'https://i.imgur.com/Bvke53p.jpg' },
   { name: 'airplane', value: 'https://farm6.staticflickr.com/5590/14821526429_5c6ea60405_z_d.jpg' },
   { name: 'cello', value: 'https://farm2.staticflickr.com/1090/4595137268_0e3f2b9aa7_z_d.jpg' },
   { name: 'violin', value: 'https://farm6.staticflickr.com/5076/7175144508_e1ba0d6e35_z_d.jpg' },
