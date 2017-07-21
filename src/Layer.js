ff50f1e4fcc877b8a3a002885545df7f311d8f05
Leon Chen <leon@md.ai>
2017-07-21 17:24:50 -0400

    add reshapeTensorFromTiled method in Layer class

--- src/Layer.js
+++ src/Layer.js
@@ -27,11 +27,11 @@ export default class Layer {
    *
    * @param {Tensor[]} weightsArr - array of weights which are instances of Tensor
    */
-  setWeights(weightsArr) {
+  setWeights(weightsArr, createGLTexture = true) {
     this.params.forEach((p, i) => {
       this.weights[p] = weightsArr[i]
 
-      if (this.gpu) {
+      if (this.gpu && createGLTexture) {
         this.weights[p].createGLTexture()
       }
     })
@@ -58,6 +58,38 @@ export default class Layer {
     } else {
       this.gpu = false
     }
+  }
+
+  /**
+   * Reshapes tiled data into untiled form.
+   * Called at the end when data is read back from GPU (which is in tiled 2D format from texture)
+   *
+   * @param {Tensor} x
+   * @returns {Tensor}
+   */
+  reshapeTensorFromTiled(x) {
+    if (!x.glTextureIsTiled) {
+      throw new Error('Tensor is not in tiled format.')
+    }
+    if (!x.untiledShape) {
+      throw new Error('Tensor does not contain untiledShape.')
+    }
+
+    // last axis is channel axis
+    const nbPatches = x.tensor.shape[0]
+    const channels = x.tensor.shape[1]
+
+    let reshaped = new Tensor([], x.untiledShape)
+    let outputChannelRaveled = new Tensor([], [nbPatches])
+    let outputChannel = new Tensor([], x.untiledShape.slice(0, -1))
+    for (let n = 0; n < channels; n++) {
+      ops.assign(outputChannelRaveled.tensor, x.tensor.pick(null, n))
+      outputChannel.replaceTensorData(outputChannelRaveled.tensor.data)
+      const axisSlices = Array(x.untiledShape.length - 1).fill(null)
+      ops.assign(reshaped.tensor.pick(...axisSlices, n), outputChannel.tensor)
+    }
+
+    return reshaped
   }
 
   // /**
