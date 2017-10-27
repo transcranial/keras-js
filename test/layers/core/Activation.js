070b8dfd99596bacd29e401507640f6dd3aad2d2
Leon Chen <leon@md.ai>
2017-10-27 12:51:32 -0400

    update advanced_activations and Activation layer tests

--- test/layers/core/Activation.js
+++ test/layers/core/Activation.js
@@ -6,6 +6,19 @@ describe('core layer: Activation', function() {
   const approxEquals = KerasJS.testUtils.approxEquals
   const layers = KerasJS.layers
 
+  const activations = [
+    'softmax',
+    'elu',
+    'selu',
+    'softplus',
+    'softsign',
+    'relu',
+    'tanh',
+    'sigmoid',
+    'hard_sigmoid',
+    'linear'
+  ]
+
   before(function() {
     console.log('\n%ccore layer: Activation', styles.h1)
   })
@@ -18,44 +31,28 @@ describe('core layer: Activation', function() {
       console.log('\n%cCPU', styles.h2)
     })
 
-    it('[core.Activation.0] [CPU] should produce expected values for tanh activation following Dense layer', function() {
-      const key = 'core.Activation.0'
-      console.log(`\n%c[${key}] [CPU] test 1 (tanh)`, styles.h3)
-      let testLayer1 = new layers.Dense({ units: 2 })
-      testLayer1.setWeights(TEST_DATA[key].weights.map(w => new KerasJS.Tensor(w.data, w.shape)))
-      let t = new KerasJS.Tensor(TEST_DATA[key].input.data, TEST_DATA[key].input.shape)
-      t = testLayer1.call(t)
-      console.log('%cin', styles.h4, stringifyCondensed(t.tensor))
-      let testLayer2 = new layers.Activation({ activation: 'tanh' })
-      const startTime = performance.now()
-      t = testLayer2.call(t)
-      const endTime = performance.now()
-      console.log('%cout', styles.h4, stringifyCondensed(t.tensor))
-      logTime(startTime, endTime)
-      const dataExpected = new Float32Array(TEST_DATA[key].expected.data)
-      const shapeExpected = TEST_DATA[key].expected.shape
-      assert.deepEqual(t.tensor.shape, shapeExpected)
-      assert.isTrue(approxEquals(t.tensor, dataExpected))
-    })
+    activations.forEach((activation, i) => {
+      const key = `core.Activation.${i}`
+      const title = `[${key}] [CPU] should produce expected values for ${activation} activation following Dense layer`
 
-    it('[core.Activation.1] [CPU] should produce expected values for hard_sigmoid activation following Dense layer', function() {
-      const key = 'core.Activation.1'
-      console.log(`\n%c[${key}] [CPU] test 2 (hard_sigmoid)`, styles.h3)
-      let testLayer1 = new layers.Dense({ units: 2 })
-      testLayer1.setWeights(TEST_DATA[key].weights.map(w => new KerasJS.Tensor(w.data, w.shape)))
-      let t = new KerasJS.Tensor(TEST_DATA[key].input.data, TEST_DATA[key].input.shape)
-      t = testLayer1.call(t)
-      console.log('%cin', styles.h4, stringifyCondensed(t.tensor))
-      let testLayer2 = new layers.Activation({ activation: 'hard_sigmoid' })
-      const startTime = performance.now()
-      t = testLayer2.call(t)
-      const endTime = performance.now()
-      console.log('%cout', styles.h4, stringifyCondensed(t.tensor))
-      logTime(startTime, endTime)
-      const dataExpected = new Float32Array(TEST_DATA[key].expected.data)
-      const shapeExpected = TEST_DATA[key].expected.shape
-      assert.deepEqual(t.tensor.shape, shapeExpected)
-      assert.isTrue(approxEquals(t.tensor, dataExpected))
+      it(title, function() {
+        console.log(`\n%c[${key}] [CPU] ${activation}`, styles.h3)
+        let testLayer1 = new layers.Dense({ units: 2 })
+        testLayer1.setWeights(TEST_DATA[key].weights.map(w => new KerasJS.Tensor(w.data, w.shape)))
+        let t = new KerasJS.Tensor(TEST_DATA[key].input.data, TEST_DATA[key].input.shape)
+        t = testLayer1.call(t)
+        console.log('%cin', styles.h4, stringifyCondensed(t.tensor))
+        let testLayer2 = new layers.Activation({ activation })
+        const startTime = performance.now()
+        t = testLayer2.call(t)
+        const endTime = performance.now()
+        console.log('%cout', styles.h4, stringifyCondensed(t.tensor))
+        logTime(startTime, endTime)
+        const dataExpected = new Float32Array(TEST_DATA[key].expected.data)
+        const shapeExpected = TEST_DATA[key].expected.shape
+        assert.deepEqual(t.tensor.shape, shapeExpected)
+        assert.isTrue(approxEquals(t.tensor, dataExpected))
+      })
     })
   })
 
@@ -67,44 +64,28 @@ describe('core layer: Activation', function() {
       console.log('\n%cGPU', styles.h2)
     })
 
-    it('[core.Activation.0] [GPU] should produce expected values for tanh activation following Dense layer', function() {
-      const key = 'core.Activation.0'
-      console.log(`\n%c[${key}] [GPU] test 1 (tanh)`, styles.h3)
-      let testLayer1 = new layers.Dense({ units: 2, gpu: true })
-      testLayer1.setWeights(TEST_DATA[key].weights.map(w => new KerasJS.Tensor(w.data, w.shape)))
-      let t = new KerasJS.Tensor(TEST_DATA[key].input.data, TEST_DATA[key].input.shape)
-      t = testLayer1.call(t)
-      console.log('%cin', styles.h4, stringifyCondensed(t.tensor))
-      let testLayer2 = new layers.Activation({ activation: 'tanh', gpu: true })
-      const startTime = performance.now()
-      t = testLayer2.call(t)
-      const endTime = performance.now()
-      console.log('%cout', styles.h4, stringifyCondensed(t.tensor))
-      logTime(startTime, endTime)
-      const dataExpected = new Float32Array(TEST_DATA[key].expected.data)
-      const shapeExpected = TEST_DATA[key].expected.shape
-      assert.deepEqual(t.tensor.shape, shapeExpected)
-      assert.isTrue(approxEquals(t.tensor, dataExpected))
-    })
+    activations.forEach((activation, i) => {
+      const key = `core.Activation.${i}`
+      const title = `[${key}] [CPU] should produce expected values for ${activation} activation following Dense layer`
 
-    it('[core.Activation.1] [GPU] should produce expected values for hard_sigmoid activation following Dense layer', function() {
-      const key = 'core.Activation.1'
-      console.log(`\n%c[${key}] [GPU] test 2 (hard_sigmoid)`, styles.h3)
-      let testLayer1 = new layers.Dense({ units: 2, gpu: true })
-      testLayer1.setWeights(TEST_DATA[key].weights.map(w => new KerasJS.Tensor(w.data, w.shape)))
-      let t = new KerasJS.Tensor(TEST_DATA[key].input.data, TEST_DATA[key].input.shape)
-      t = testLayer1.call(t)
-      console.log('%cin', styles.h4, stringifyCondensed(t.tensor))
-      let testLayer2 = new layers.Activation({ activation: 'hard_sigmoid', gpu: true })
-      const startTime = performance.now()
-      t = testLayer2.call(t)
-      const endTime = performance.now()
-      console.log('%cout', styles.h4, stringifyCondensed(t.tensor))
-      logTime(startTime, endTime)
-      const dataExpected = new Float32Array(TEST_DATA[key].expected.data)
-      const shapeExpected = TEST_DATA[key].expected.shape
-      assert.deepEqual(t.tensor.shape, shapeExpected)
-      assert.isTrue(approxEquals(t.tensor, dataExpected))
+      it(title, function() {
+        console.log(`\n%c[${key}] [GPU] ${activation}`, styles.h3)
+        let testLayer1 = new layers.Dense({ units: 2, gpu: true })
+        testLayer1.setWeights(TEST_DATA[key].weights.map(w => new KerasJS.Tensor(w.data, w.shape)))
+        let t = new KerasJS.Tensor(TEST_DATA[key].input.data, TEST_DATA[key].input.shape)
+        t = testLayer1.call(t)
+        console.log('%cin', styles.h4, stringifyCondensed(t.tensor))
+        let testLayer2 = new layers.Activation({ activation, gpu: true })
+        const startTime = performance.now()
+        t = testLayer2.call(t)
+        const endTime = performance.now()
+        console.log('%cout', styles.h4, stringifyCondensed(t.tensor))
+        logTime(startTime, endTime)
+        const dataExpected = new Float32Array(TEST_DATA[key].expected.data)
+        const shapeExpected = TEST_DATA[key].expected.shape
+        assert.deepEqual(t.tensor.shape, shapeExpected)
+        assert.isTrue(approxEquals(t.tensor, dataExpected))
+      })
     })
   })
 })
