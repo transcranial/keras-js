fd383ea31ffb0376ecb09cf4912a3fea9c5f1e75
Leon Chen <leon@md.ai>
2017-11-01 22:00:25 -0400

    add gpu support for Permute layer

--- src/layers/core/Permute.js
+++ src/layers/core/Permute.js
@@ -1,4 +1,8 @@
 import Layer from '../../Layer'
+import Tensor from '../../Tensor'
+import { webgl2 } from '../../WebGL2'
+import _ from 'lodash'
+import ops from 'ndarray-ops'
 
 /**
  * Permute layer class
@@ -16,20 +20,139 @@ export default class Permute extends Layer {
 
     const { dims = [] } = attrs
     this.dims = dims.map(dim => dim - 1)
+
+    // GPU setup
+    if (this.gpu) {
+      this.mapInputProgram = webgl2.compileProgram(require('../../mapInput.webgl2.glsl'))
+    }
   }
 
   /**
-   * Method for layer computational logic
+   * Layer computational logic
+   *
    * @param {Tensor} x
-   * @returns {Tensor} x
+   * @returns {Tensor}
    */
   call(x) {
+    if (x.tensor.shape.length <= 1 || _.isEqual(_.range(x.tensor.shape.length), this.dims)) {
+      return x
+    }
+
+    if (this.gpu) {
+      this._call_gpu(x)
+    } else {
+      this._call_cpu(x)
+    }
+    return this.output
+  }
+
+  /**
+   * CPU call
+   */
+  _call_cpu(x) {
     if (this.dims.length !== x.tensor.shape.length) {
       throw new Error(
         `${this.name} [Permute layer] The specified dims permutation must match the number of dimensions.`
       )
     }
-    x.tensor = x.tensor.transpose(...this.dims)
-    return x
+
+    const outputShape = this.dims.map(i => x.tensor.shape[i])
+    this.output = new Tensor([], outputShape)
+    ops.assign(this.output.tensor, x.tensor.transpose(...this.dims))
+  }
+
+  _createIndexMap(inputShape) {
+    if (this.rowIndexMap && this.colIndexMap) {
+      return
+    }
+
+    let indicesRow = new Tensor([], inputShape)
+    let indicesCol = new Tensor([], inputShape)
+
+    if (inputShape.length === 2) {
+      for (let i = 0; i < inputShape[0]; i++) {
+        ops.assigns(indicesRow.tensor.pick(i, null), i)
+      }
+    } else if (inputShape.length === 3) {
+      for (let i = 0; i < inputShape[0]; i++) {
+        for (let j = 0; j < inputShape[1]; j++) {
+          ops.assigns(indicesRow.tensor.pick(i, j, null), i * inputShape[1] + j)
+        }
+      }
+    } else if (inputShape.length === 4) {
+      for (let i = 0; i < inputShape[0]; i++) {
+        for (let j = 0; j < inputShape[1]; j++) {
+          for (let k = 0; k < inputShape[2]; k++) {
+            ops.assigns(
+              indicesRow.tensor.pick(i, j, k, null),
+              i * inputShape[1] * inputShape[2] + j * inputShape[2] + k
+            )
+          }
+        }
+      }
+    }
+    for (let c = 0; c < _.last(inputShape); c++) {
+      ops.assigns(indicesCol.tensor.pick(...Array(inputShape.length - 1).fill(null), c), c)
+    }
+
+    const outputShape = this.dims.map(i => inputShape[i])
+    this.rowIndexMap = new Tensor([], outputShape, { type: Int32Array })
+    this.colIndexMap = new Tensor([], outputShape, { type: Int32Array })
+    ops.assign(this.rowIndexMap.tensor, indicesRow.tensor.transpose(...this.dims))
+    ops.assign(this.colIndexMap.tensor, indicesCol.tensor.transpose(...this.dims))
+    if (outputShape.length > 2) {
+      this.rowIndexMap.reshapeTensorToTiled()
+      this.colIndexMap.reshapeTensorToTiled()
+    }
+
+    if (this.gpu) {
+      this.rowIndexMap.createGLTexture('2d', 'int')
+      this.colIndexMap.createGLTexture('2d', 'int')
+    }
+  }
+
+  /**
+   * GPU call
+   */
+  _call_gpu(x) {
+    if (!x.glTexture) {
+      this.inputShape = x.tensor.shape
+      if (x.tensor.shape.length <= 2) {
+        x.createGLTexture()
+      } else if (x.tensor.shape.length > 2 && !x.glTextureIsTiled) {
+        x.reshapeTensorToTiled()
+        x.createGLTexture()
+      }
+    } else if (x.glTextureIsTiled) {
+      this.inputShape = x.untiledShape
+    } else {
+      this.inputShape = x.tensor.shape
+    }
+    this._createIndexMap(this.inputShape)
+
+    if (!this.output) {
+      const outputShape = this.dims.map(i => this.inputShape[i])
+      this.output = new Tensor([], outputShape)
+      if (outputShape.length > 2) {
+        this.output.reshapeTensorToTiled()
+      }
+      this.output.createGLTexture()
+    }
+
+    webgl2.selectProgram(this.mapInputProgram)
+    webgl2.bindOutputTexture(this.output.glTexture, this.output.glTextureShape)
+    let textures = [x.glTexture, this.rowIndexMap.glTexture, this.colIndexMap.glTexture]
+    let textureTypes = ['2d', '2d', '2d']
+    let textureNames = ['x', 'rowIndexMap', 'colIndexMap']
+    webgl2.bindInputTextures(this.mapInputProgram, textures, textureTypes, textureNames)
+    webgl2.runProgram()
+
+    // GPU -> CPU data transfer
+    if (this.outbound.length === 0) {
+      this.output.transferFromGLTexture()
+      if (this.output.glTextureIsTiled) {
+        this.output.reshapeTensorFromTiled(this.axis)
+      }
+    }
   }
 }
--- test/layers/core/Permute.js
+++ test/layers/core/Permute.js
@@ -10,37 +10,89 @@ describe('core layer: Permute', function() {
     console.log('\n%ccore layer: Permute', styles.h1)
   })
 
-  it('[core.Permute.0] should be able to go from shape [3, 2] -> [2, 3]', function() {
-    const key = 'core.Permute.0'
-    console.log(`\n%c[${key}] shape [3, 2] -> [2, 3]`, styles.h3)
-    let testLayer = new layers.Permute({ dims: [2, 1] })
-    let t = new KerasJS.Tensor(TEST_DATA[key].input.data, TEST_DATA[key].input.shape)
-    console.log('%cin', styles.h4, stringifyCondensed(t.tensor))
-    const startTime = performance.now()
-    t = testLayer.call(t)
-    const endTime = performance.now()
-    console.log('%cout', styles.h4, stringifyCondensed(t.tensor))
-    logTime(startTime, endTime)
-    const dataExpected = new Float32Array(TEST_DATA[key].expected.data)
-    const shapeExpected = TEST_DATA[key].expected.shape
-    assert.deepEqual(t.tensor.shape, shapeExpected)
-    assert.isTrue(approxEquals(t.tensor, dataExpected))
+  /*********************************************************
+  * CPU
+  *********************************************************/
+  describe('CPU', function() {
+    before(function() {
+      console.log('\n%cCPU', styles.h2)
+    })
+
+    it('[core.Permute.0] should be able to go from shape [3, 2] -> [2, 3]', function() {
+      const key = 'core.Permute.0'
+      console.log(`\n%c[${key}] [CPU] shape [3, 2] -> [2, 3]`, styles.h3)
+      let testLayer = new layers.Permute({ dims: [2, 1] })
+      let t = new KerasJS.Tensor(TEST_DATA[key].input.data, TEST_DATA[key].input.shape)
+      console.log('%cin', styles.h4, stringifyCondensed(t.tensor))
+      const startTime = performance.now()
+      t = testLayer.call(t)
+      const endTime = performance.now()
+      console.log('%cout', styles.h4, stringifyCondensed(t.tensor))
+      logTime(startTime, endTime)
+      const dataExpected = new Float32Array(TEST_DATA[key].expected.data)
+      const shapeExpected = TEST_DATA[key].expected.shape
+      assert.deepEqual(t.tensor.shape, shapeExpected)
+      assert.isTrue(approxEquals(t.tensor, dataExpected))
+    })
+
+    it('[core.Permute.1] should be able to go from shape [2, 3, 4] -> [4, 3, 2]', function() {
+      const key = 'core.Permute.1'
+      console.log(`\n%c[${key}] [CPU] shape [2, 3, 4] -> [4, 3, 2]`, styles.h3)
+      let testLayer = new layers.Permute({ dims: [3, 2, 1] })
+      let t = new KerasJS.Tensor(TEST_DATA[key].input.data, TEST_DATA[key].input.shape)
+      console.log('%cin', styles.h4, stringifyCondensed(t.tensor))
+      const startTime = performance.now()
+      t = testLayer.call(t)
+      const endTime = performance.now()
+      console.log('%cout', styles.h4, stringifyCondensed(t.tensor))
+      logTime(startTime, endTime)
+      const dataExpected = new Float32Array(TEST_DATA[key].expected.data)
+      const shapeExpected = TEST_DATA[key].expected.shape
+      assert.deepEqual(t.tensor.shape, shapeExpected)
+      assert.isTrue(approxEquals(t.tensor, dataExpected))
+    })
   })
 
-  it('[core.Permute.1] should be able to go from shape [2, 3, 4] -> [4, 3, 2]', function() {
-    const key = 'core.Permute.1'
-    console.log(`\n%c[${key}] shape [2, 3, 4] -> [4, 3, 2]`, styles.h3)
-    let testLayer = new layers.Permute({ dims: [3, 2, 1] })
-    let t = new KerasJS.Tensor(TEST_DATA[key].input.data, TEST_DATA[key].input.shape)
-    console.log('%cin', styles.h4, stringifyCondensed(t.tensor))
-    const startTime = performance.now()
-    t = testLayer.call(t)
-    const endTime = performance.now()
-    console.log('%cout', styles.h4, stringifyCondensed(t.tensor))
-    logTime(startTime, endTime)
-    const dataExpected = new Float32Array(TEST_DATA[key].expected.data)
-    const shapeExpected = TEST_DATA[key].expected.shape
-    assert.deepEqual(t.tensor.shape, shapeExpected)
-    assert.isTrue(approxEquals(t.tensor, dataExpected))
+  /*********************************************************
+  * GPU
+  *********************************************************/
+  describe('GPU', function() {
+    before(function() {
+      console.log('\n%cGPU', styles.h2)
+    })
+
+    it('[core.Permute.0] should be able to go from shape [3, 2] -> [2, 3]', function() {
+      const key = 'core.Permute.0'
+      console.log(`\n%c[${key}] [GPU] shape [3, 2] -> [2, 3]`, styles.h3)
+      let testLayer = new layers.Permute({ dims: [2, 1], gpu: true })
+      let t = new KerasJS.Tensor(TEST_DATA[key].input.data, TEST_DATA[key].input.shape)
+      console.log('%cin', styles.h4, stringifyCondensed(t.tensor))
+      const startTime = performance.now()
+      t = testLayer.call(t)
+      const endTime = performance.now()
+      console.log('%cout', styles.h4, stringifyCondensed(t.tensor))
+      logTime(startTime, endTime)
+      const dataExpected = new Float32Array(TEST_DATA[key].expected.data)
+      const shapeExpected = TEST_DATA[key].expected.shape
+      assert.deepEqual(t.tensor.shape, shapeExpected)
+      assert.isTrue(approxEquals(t.tensor, dataExpected))
+    })
+
+    it('[core.Permute.1] should be able to go from shape [2, 3, 4] -> [4, 3, 2]', function() {
+      const key = 'core.Permute.1'
+      console.log(`\n%c[${key}] [GPU] shape [2, 3, 4] -> [4, 3, 2]`, styles.h3)
+      let testLayer = new layers.Permute({ dims: [3, 2, 1], gpu: true })
+      let t = new KerasJS.Tensor(TEST_DATA[key].input.data, TEST_DATA[key].input.shape)
+      console.log('%cin', styles.h4, stringifyCondensed(t.tensor))
+      const startTime = performance.now()
+      t = testLayer.call(t)
+      const endTime = performance.now()
+      console.log('%cout', styles.h4, stringifyCondensed(t.tensor))
+      logTime(startTime, endTime)
+      const dataExpected = new Float32Array(TEST_DATA[key].expected.data)
+      const shapeExpected = TEST_DATA[key].expected.shape
+      assert.deepEqual(t.tensor.shape, shapeExpected)
+      assert.isTrue(approxEquals(t.tensor, dataExpected))
+    })
   })
 })
