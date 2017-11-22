40cff436022a58c83e4b55eebbe7ae8004822e64
Leon Chen <leon@md.ai>
2017-11-22 10:27:52 -0500

    export GPU_SUPPORT constant

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import '@babel/polyfill'
 import Model from './Model'
 import Tensor from './Tensor'
+import { webgl2 } from './WebGL2'
 import * as activations from './activations'
 import * as layers from './layers'
 import * as testUtils from './utils/testUtils'
 
-export { Model, Tensor, activations, layers, testUtils }
+const GPU_SUPPORT = webgl2.isSupported
+
+export { Model, Tensor, GPU_SUPPORT, activations, layers, testUtils }
