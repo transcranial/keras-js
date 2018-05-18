4fc2513905ac075954c3ea7c1ef55a1fae1f54f6
Sinan Yasar <sinan@koding.com>
2018-05-18 00:55:13 -0700

    fix babel polyfill for thirdparties #109

---
+++ demos/src/demos.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import App from './App'
+import router from './router'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#1bbc9b',
+    secondary: '#69707a',
+    accent: '#f5d76e',
+    error: '#d24d57'
+  }
+})
+
+const app = new Vue(Object.assign({ router }, App))
+
+app.$mount('#root')
--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
-import '@babel/polyfill'
-import Model from './Model'
-import Tensor from './Tensor'
-import { webgl2 } from './WebGL2'
-import * as activations from './activations'
-import * as layers from './layers'
-import * as testUtils from './utils/testUtils'
-
-const GPU_SUPPORT = webgl2.isSupported
-
-export { Model, Tensor, GPU_SUPPORT, activations, layers, testUtils }
-
-const KerasJS = { Model, Tensor, GPU_SUPPORT, activations, layers, testUtils }
-export { KerasJS as default }
+if (!global._babelPolyfill) {
+  require('@babel/polyfill')
+}
+import './keras'
